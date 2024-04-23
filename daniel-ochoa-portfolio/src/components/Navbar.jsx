63997244b0c894f9fb3d61ae7c42c8ef594ab589
DanOcho8 <98377769+DanOcho8@users.noreply.github.com>
2024-04-22 21:27:15 -0700

    Added a navbar and used FaBars and FaTimes for the mobile hamburger menu

--- daniel-ochoa-portfolio/src/components/Navbar.jsx
+++ daniel-ochoa-portfolio/src/components/Navbar.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-
+import React, {useState} from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/daniel.png";
 const Navbar = () => {
-    return (
-        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]">
-            
-        </div>
-    )
-}
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
 
-export default Navbar
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#000000] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo Image" style={{ width: "60px" }} />
+      </div>
+
+        <ul className="hidden md:flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile */}
+      <ul className={!nav ? 'hidden': "absolute top-0 left-o w-full h-screen bg-[#000000] flex flex-col justify-center items-center"}>
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Projects</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* Social Icons */}
+      <div className="hidden"></div>
+    </div>
+  );
+};
+
+export default Navbar;

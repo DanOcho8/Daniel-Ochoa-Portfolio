d3a7eda41fcb8869073df0146957d3f3a9ebe795
DanOcho8 <98377769+DanOcho8@users.noreply.github.com>
2024-04-24 19:41:03 -0700

    updated some styles, would like to add some more near future.

--- daniel-ochoa-portfolio/src/components/Contact.jsx
+++ daniel-ochoa-portfolio/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
         className="flex flex-col max-w-[600px] w-full"
       >
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+          <p className="text-4xl font-bold inline border-b-4 border-[#357a3a] text-gray-300">
             Contact
           </p>
           <p className="text-gray-300 py-4">
@@ -37,7 +37,7 @@ const Contact = () => {
           rows="10"
           placeholder="Message"
         />
-        <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+        <button className="text-white border-2 hover:bg-[#357a3a] hover:border-[#357a3a] px-4 py-3 my-8 mx-auto flex items-center">
           Send
         </button>
       </form>
--- daniel-ochoa-portfolio/src/components/Home.jsx
+++ daniel-ochoa-portfolio/src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[black]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-white">
-        <p className="text-pink-600">Hi, my name is</p>
+        <p className="text-[#357a3a]">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Daniel Ochoa
         </h1>
@@ -23,7 +23,7 @@ const Home = () => {
         <div>
           <button
             href=""
-            className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600"
+            className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#357a3a] hover:border-[#357a3a]"
           >
             <Link to="work" smooth={true} duration={500}>
               View Work
